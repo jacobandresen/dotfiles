dd61614933c648555b5131818b93a75980be1fb9
Jacob Andresen <jacob.andresen@gmail.com>
2026-05-10 10:41:41 +0200

    add fzf to setup.sh for all platforms

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -5,16 +5,16 @@ IS_WSL=0
 case "$(uname -r)" in *microsoft* | *Microsoft*) IS_WSL=1 ;; esac
 
 if [ "$(uname)" = "Darwin" ]; then
-  brew install neovim make gcc node python jq git fpc
+  brew install neovim make gcc node python jq git fpc fzf
 elif [ -f /etc/arch-release ]; then
-  sudo pacman -S --needed neovim ttf-terminus-nerd base-devel make gcc nodejs npm python jq git fpc wl-clipboard
+  sudo pacman -S --needed neovim ttf-terminus-nerd base-devel make gcc nodejs npm python jq git fpc fzf wl-clipboard
 elif [ -f /etc/debian_version ]; then
   sudo apt-get update
   # The apt neovim is often outdated; use the upstream PPA for a recent stable release
   sudo apt-get install -y software-properties-common
   sudo add-apt-repository -y ppa:neovim-ppa/stable
   sudo apt-get update
-  sudo apt-get install -y neovim build-essential make gcc nodejs npm python3 jq git fpc
+  sudo apt-get install -y neovim build-essential make gcc nodejs npm python3 jq git fpc fzf
   if [ "$IS_WSL" = "1" ]; then
     echo "Note: install the Nerd Font on your Windows host (not inside WSL)."
     echo "Download from https://www.nerdfonts.com/ and install via Windows Font Settings."
