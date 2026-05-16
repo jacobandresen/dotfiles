1eebe343da097e8bad264b8a134e15af3b15d91d
Jacob Andresen <jacob.andresen@gmail.com>
2026-05-16 11:04:40 +0200

    add some more example tests

--- tests/test-turbo-ralph.sh
+++ tests/test-turbo-ralph.sh
@@ -7,9 +7,7 @@ rm -Rf ~/.ralph
 
 rm -Rf /tmp/ralph
 mkdir -p /tmp/ralph
-echo "--- test1 :"
 turbo-ralph.sh --dir "/tmp/ralph/helloworld" "write helloworld"
-echo "--- test2 :"
-turbo-ralph.sh --dir "/tmp/ralph/fibonacci" "write the fibonacci sequence using c#"
-echo "--- test3 :"
-turbo-ralph.sh --dir "/tmp/ralph/line" "render a line on screen via SDL2. Use sdl2-config"
+turbo-ralph.sh --dir "/tmp/ralph/line" "render a line on screen via SDL2. Use sdl2-config in the Makefile to setup SDL2 libs"
+turbo-ralph.sh --dir "/tmp/ralph/fibonacci" "write the fibonacci sequence using c#. Use the dotnet command to compile c#"
+turbo-ralph.sh --dir "/tmp/ralph/pythondata" "write a python program that a todo  entry to a sqlite3 database with list of todos. create the todo table in the sqlite3 databasea via python. Show that the inserted entry can be read again"
