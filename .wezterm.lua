a43ffe04f2941938fce658d8c86fec8cb14e8c20
Jacob Andresen <jacob.andresen@gmail.com>
2026-05-24 09:10:15 +0200

    wezterm Ascendancy

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,7 @@ local config = wezterm.config_builder()
 config.initial_cols = 120
 config.initial_rows = 40
 
-config.color_scheme = "Aztec (base16)"
+config.color_scheme = "Ascendancy (base16)"
 
 config.font_size = 14
 
